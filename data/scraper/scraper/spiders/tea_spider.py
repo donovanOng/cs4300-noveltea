1c6e01e43357191a18a8acedf0a96c6287f5a0e4
Donovan Ong <donovan.ongyl@gmail.com>
2018-03-21 19:50:50 -0400

    Scraper using scrapy

---
+++ data/scraper/scraper/spiders/tea_spider.py
@@ -0,0 +1,42 @@
+import scrapy
+
+
+class TeaSpider(scrapy.Spider):
+    name = 'tea'
+
+    start_urls = ['https://steepster.com/teas']
+
+    def parse(self, response):
+        # follow links to tea pages
+        for href in response.css('a.tea-name::attr(href)'):
+            yield response.follow(href, self.parse_tea)
+
+        # follow pagination links
+        pagination_links = response.css('nav.pagination ul li a::attr(href)').extract()
+        yield response.follow(pagination_links[-1], self.parse)
+
+    def parse_tea(self, response):
+        def extract_with_css(query):
+            return response.css(query).extract_first().strip()
+
+        tea_description = response.css('dl.tea-description dd::text')
+        tea_array = []
+        for row in tea_description:
+            tea_array.append(row.extract().strip())
+
+        yield {
+            'id': response.url.split('/')[5].split("-")[0],
+            'name': extract_with_css('h1[itemprop=name]::text'),
+            'brand': extract_with_css('span[itemprop=brand] span[itemprop=name]::text'),
+            'reviewCount': extract_with_css('meta[itemprop=reviewCount]::attr(content)'),
+            'ratingValue': extract_with_css('div[itemprop=ratingValue]::text'),
+            'ingredients': tea_array[0],
+            'flavors': tea_array[1],
+            'soldIn': tea_array[2],
+            'caffeine': tea_array[3],
+            'certification': tea_array[4],
+            'wantIt': extract_with_css('section[id=want-user-images] h3::text').replace(" Want it", ""),
+            'ownIt': extract_with_css('section[id=have-user-images] h3::text').replace(" Own it", ""),
+            'url': response.url
+        }
+        # TODO: Get tea image
